3691d96efb499b056a6d2712f26b21100f187832
n.akhmetov <n.akhmetov@corp.mail.ru>
2018-04-23 18:00:17 +0300

    Figma to XFL generation tests.

--- src/Figma.hx
+++ src/Figma.hx
@@ -5,31 +5,23 @@ import haxe.MainLoop;
 import sys.io.File;
 
 using Figma;
+using haxe.Json;
 
 class Figma {
 
 	public static function main():Void {
-		/*var figmaAPI:FigmaAPI = new FigmaAPI("token".load());
+		var figmaAPI:FigmaAPI = new FigmaAPI("token".load());
 		figmaAPI.files("file".load(), function(r:Response<Document>) {
-			"xfl/Test2.json".save(Json.stringify(r.data));
-			if (r.data != null) {
-				trace(r.data.name);
-				trace(r.data.lastModified);
-				var document:DocumentNode = r.data.document;
-				for (node in document.children) trace(node.type);
-			} else {
-				trace(r.error);
-				trace(r.error.err);
-				trace(r.error.status);
-			}
+			'xfl/${r.data.name}.json'.save(Json.stringify(r.data));
+			FigmaXFLGenerate.generate(r.data, "xfl");
 		});
 
-		MainLoop.addThread(keep);*/
+		MainLoop.addThread(keep);
 		
 		//FigmaXFL.update(cast "xfl/Test.json".load(), "xfl/Test");
 		//FigmaXFLUpdate.update(cast "xfl/Test.json".load(), "xfl/Test");
-		
-		FigmaXFLGenerate.generate(cast "xfl/Test2.json".load(), "xfl");
+
+		//FigmaXFLGenerate.generate("xfl/Test2.json".load().parse(), "xfl");
 	}
 
 	public static inline function load(file:String):String return File.getContent(file);
--- src/FigmaXFLGenerate.hx
+++ src/FigmaXFLGenerate.hx
@@ -1,38 +1,67 @@
 package ;
-import figma.FigmaAPI;
+import figma.FigmaAPI.NodeType;
 import figma.FigmaAPI;
 import haxe.Template;
 
 using Figma;
 using FigmaXFLGenerate;
+using sys.FileSystem;
+using haxe.xml.Printer;
+using Xml;
+using StringTools;
 
 class FigmaXFLGenerate {
-	
+
 	private static inline var MAIN:String = "Main";
-	
+
 	public function new():Void {}
 
+	private var xflRoot:String;
+	private var xflPath:String;
+	private var libraryPath:String;
+
 	public var filename:String;
-	
-	public var width:Int;
-	public var height:Int;
+
+	public var width:Float;
+	public var height:Float;
 	public var backgroundColor:String;
-	
+
 	public var symbols:Array<Symbol>;
 	public var timelines:Array<Timeline>;
 
+	private var symbolsMap:Map<String, Symbol>;
+
 	public function execute(content:Document, xflRoot:String) {
+
+		this.xflRoot = xflRoot;
+
+		filename = content.name;
+
+		xflPath = '$xflRoot/$filename';
+		libraryPath = '$xflPath/LIBRARY';
+
+		var xfl:String = '$xflPath/$filename.xfl';
+		if (!xfl.exists()) xfl.save('$xflRoot/template/Name.xfl'.load());
+
+		if (!xflPath.exists()) xflPath.createDirectory();
+		if (!libraryPath.exists()) libraryPath.createDirectory();
+
 		parse(content);
-		'$xflRoot/$filename/DOMDocument.xmlt'.save(
-			new Template('$xflRoot/template/DOMDocument.xmlt'.load()).execute(this, this)
+
+		trace(timelines);
+
+		'$xflPath/DOMDocument.xml'.save(
+			new Template('$xflRoot/template/DOMDocument.xmlt'.load()).execute(this, this).pretty()
 		);
 	}
 
 	private function parse(content:Document):Void {
-		
-		filename = content.name;
+
+		symbolsMap = new Map<String, Symbol>();
+
+		symbols = [];
 		timelines = [];
-		
+
 		for (node in content.document.children) {
 			if (node.type == NodeType.Canvas) {
 				var canvas:CanvasNode = cast node;
@@ -43,18 +72,10 @@ class FigmaXFLGenerate {
 						if (frameNode.name == MAIN) setMain(cast frameNode);
 						var frame:Frame = { elements:[] };
 						var layer:Layer = { frames:[frame], name:frameNode.name };
-						for (node in frameNode.children) {
-							switch (node.type) {
-								case NodeType.Component, NodeType.Instance: {
-									if (node.type == NodeType.Component) addLibraryItem(cast node);
-									frame.elements.push({ type:ElementType.SymbolInstance });
-								}
-								default:
-							}
-						}
+						getElements(frame, frameNode.children);
 						timeline.layers.push(layer);
 					} else {
-						trace("Error! Unframed elements not allowed!")
+						trace("Error! Unframed elements not allowed!");
 					}
 				}
 				timelines.push(timeline);
@@ -62,14 +83,79 @@ class FigmaXFLGenerate {
 		}
 	}
 
-	private function addLibraryItem(component:ComponentNode):Symbol {
-		return symbols.push({ type:Include, href:'${component.name}.xfl', itemID:guid() });
+	private function getElements(frame:Frame, children:Array<Node>):Void {
+		for (node in children) getElement(frame, node);
+	}
+
+	private function getElement(frame:Frame, node:Node):Void {
+		var element:Element = switch (node.type) {
+			case NodeType.Component, NodeType.Instance: {
+				checkSymbol(cast node);
+				var symbolElement:SymbolElement = { type:ElementType.SymbolInstance, libraryItemName:node.name };
+				symbolElement;
+			}
+			case NodeType.Rectangle: {
+				var rect:RectangleNode = cast node;
+				var rectangle:RectangleElement = { type:ElementType.Rectangle, width:rect.absoluteBoundingBox.width, height:rect.absoluteBoundingBox.height };
+				if (rect.fills.isNotEmpty()) rectangle.fill = rect.fills[0].color.hexColor();
+				rectangle;
+			}
+			default: null;
+		}
+		if (element != null) {
+			switch (node.type) {
+				case NodeType.Component, NodeType.Instance, NodeType.Vector: {
+					var vectorNode:FrameNode = cast node;
+					if (vectorNode.absoluteBoundingBox.x != 0 || vectorNode.absoluteBoundingBox.y != 0) {
+						element.matrix = { tx:vectorNode.absoluteBoundingBox.x, ty:vectorNode.absoluteBoundingBox.y };
+					}
+				}
+				default:
+			}
+			frame.elements.push(element);
+		}
+	}
+
+	private function getFills(fills:Array<Paint>):Array<ShapeFill> {
+		var shapeFills:Array<ShapeFill> = [];
+		var index:Int = 0;
+		for (paint in fills) shapeFills.push({ index:index++, color:paint.color.hexColor() });
+		return shapeFills;
+	}
+
+	private function checkSymbol(node:FrameNode):Symbol {
+		return symbolsMap.exists(node.name) ? symbolsMap.get(node.name) : createSymbol(
+			symbols.pushes(symbolsMap.sets(node.name, { type:SymbolType.Include, href:node.name, itemID:guid() })), node
+		);
+	}
+
+	private function createSymbol(symbol:Symbol, frameNode:FrameNode):Symbol {
+
+		var item:LibrarySymbol = { name:symbol.href, itemID:symbol.itemID, layers:[] };
+
+		for (node in frameNode.children) {
+			var frame:Frame = { elements:[] };
+			var layer:Layer = { frames:[frame], name:frameNode.name };
+			getElement(frame, node);
+			item.layers.push(layer);
+		}
+
+		trace(item);
+
+		'$libraryPath/${symbol.href}.xml'.save(
+			new Template('$xflRoot/template/LIBRARY/Item.xmlt'.load()).execute(item, this).pretty()
+		);
+		return symbol;
+	}
+
+	public function setElements(resolve:String -> Dynamic, elements:Array<Element>):String {
+		return new Template('$xflRoot/template/Elements.xmlt'.load()).execute({ elements:elements }, this).pretty();
 	}
 
 	public function setMain(frame:FrameNode):Void {
 		width = frame.absoluteBoundingBox.width;
 		height = frame.absoluteBoundingBox.height;
-		backgroundColor = frame.backgroundColor;
+		backgroundColor = frame.backgroundColor.hexColor();
 	}
 
 	public static function generate(content:Document, xflRoot:String):Void {
@@ -82,8 +168,38 @@ class FigmaXFLGenerate {
 			if (j == 8) result += "-";
 			result += StringTools.hex(Math.floor(Math.random() * 16));
 		}
-		return result;
+		return result.toLowerCase();
 	}
+
+	public static inline function sets<K, V>(map:Map<K, V>, key:K, value:V):V {
+		map.set(key, value);
+		return value;
+	}
+
+	public static inline function pushes<V>(array:Array<V>, value:V):V {
+		array.push(value);
+		return value;
+	}
+
+	public static inline function pretty(xml:String):String {
+		return ~/>\s*</g.replace(xml, "><").parse().print(true);
+	}
+
+	public static inline function hexColor(c:Color):String {
+		return '#${c.r.f2h()}${c.g.f2h()}${c.b.f2h()}';
+	}
+
+	public static inline function f2h(float:Float, base:Int = 255):String {
+		return Std.int(float * base).hex();
+	}
+
+	public static inline function isNotEmpty<T>(array:Array<T>):Bool return array != null && array.length > 0;
+}
+
+typedef LibrarySymbol = {
+	var name:String;
+	var itemID:String;
+	var layers:Array<Layer>;
 }
 
 typedef Symbol = {
@@ -116,8 +232,34 @@ typedef Frame = {
 
 typedef Element = {
 	var type:ElementType;
+	@:optional var matrix:{ tx:Float, ty:Float };
+}
+
+typedef SymbolElement = { > Element,
+	var libraryItemName:String;
+}
+
+typedef ShapeElement = { > Element,
+	var fills:Array<ShapeFill>;
+	var edges:Array<ShapeEdge>;
+}
+
+typedef ShapeFill = {
+
+}
+
+typedef ShapeEdge = {
+
+}
+
+typedef RectangleElement = { > Element,
+	var width:Float;
+	var height:Float;
+	@:optional var fill:String;
 }
 
 @:enum abstract ElementType(String) {
 	var SymbolInstance = "SymbolInstance";
+	var Shape = "Shape";
+	var Rectangle = "Rectangle";
 }
